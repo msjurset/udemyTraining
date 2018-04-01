472617af8af5623cf394dc7ae2a7e91587f05d1b
Mark Sjurseth <mark@sjurseth.com>
2018-04-01 19:49:01 -0400

    First commit

---
+++ package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/msjurset/udemyTraining/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
